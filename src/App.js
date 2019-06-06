a96573e5969e75ff98af30d468ff029ee6bb79e4
Chris Webb <kungpochicken@gmail.com>
2019-06-06 20:16:58 +1000

    lgr early version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import AdaptiveListContainer from "./adaptive-list/AdaptiveListContainer";
+import { lgrBrowser } from "./lgr/lgr";
 
 const style = {
   width: "100%",
   height: "100%"
 };
+
+let lgr = lgrBrowser();
+
+lgr.log(process);
+
+lgr.debug("hello");
+lgr({ smile: true }).debug("hello");
+lgr({ meta: "somelogger" }).debug("hello");
+lgr({ meta: ["somelogger", "info"], backgroundColor: "#FF0000" }).debug(
+  "hello"
+);
 
 function App() {
   return (
---
+++ src/lgr/htmlDomWriter.js
@@ -0,0 +1,36 @@
+const consoleWriter = () => {
+  const makeStyle = (backgroundColor = "black", color = "white") => {
+    return `background-color:${backgroundColor};color:${color};border-radius:3px;padding:2px 4px;margin-left:2px;`;
+  };
+  const transformConsoleArgs = config => (...args) => {
+    if (config === undefined) return args;
+
+    if (config.meta) {
+      const metaArgs =
+        typeof config.meta === "string"
+          ? [`%c${config.meta}%c${config.meta}`, makeStyle(), makeStyle()]
+          : [
+              config.meta.map(meta => `%c${meta}`).join(""),
+              ...config.meta.map(meta => makeStyle())
+            ];
+
+      //console.log("ARGDEBUG", [...metaArgs, ...args]);
+
+      return [...metaArgs, ...args];
+    }
+
+    return args;
+  };
+
+  return {
+    isAvailableInEnvironment: () => {
+      return window !== undefined && window.document !== undefined;
+    },
+    initialise: () => {},
+    dispatch: (level, config) => (...args) => {
+      console[level](...transformConsoleArgs(config)(...args));
+    }
+  };
+};
+
+export default consoleWriter;
